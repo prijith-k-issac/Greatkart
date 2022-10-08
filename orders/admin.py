bfec146fba24b64c4650e7231be622b4517c595e
prijith-k-issac <prijithkissac@gmail.com>
2022-10-08 06:13:26 +0530

    Order completion page

--- orders/admin.py
+++ orders/admin.py
@@ -3,6 +3,19 @@ from .models import Payment,Order,OrderProduct
 
 # Register your models here.
 
+class OrderProductInline(admin.TabularInline):
+    model=OrderProduct
+    readonly_fields =('payment','user','product','quantity','ordered')
+    extra=0
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display=['order_number','first_name','email','phone','city','status','is_ordered','created_at']
+    list_filter =['status','is_ordered']
+    search_fields = ['first_name','last_name','phone','email']
+    list_per_page = 20
+    inlines=[OrderProductInline]
+    
+    
 admin.site.register(Payment)
-admin.site.register(Order)
+admin.site.register(Order,OrderAdmin)
 admin.site.register(OrderProduct)
